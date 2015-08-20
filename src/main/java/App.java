4820437743e6854a7cbc1c06a3c50c190e9384ca
teresa fletcher & jocelyn alsdorf <student@epicodus.com>
2015-08-20 16:22:49 -0700

    route not following to display contact info from name link

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -68,5 +68,12 @@ public class App {
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
 
+   get("/contacts/:id", (request, response) -> {
+   HashMap<String, Object> model = new HashMap<String, Object>();
+   Contact contact = Contact.find(Integer.parseInt(request.params(":id")));
+   model.put("contact", contact);
+   model.put("template", "templates/contact.vtl");
+   return new ModelAndView(model, layout);
+ }, new VelocityTemplateEngine());
 }
 }
